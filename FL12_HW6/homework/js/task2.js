4351e72d48f93a1b08928989650a656a26dab302
VasylKravchenko <vasylkravchenko@gmail.com>
2019-12-20 01:11:36 +0200

    hw6 task2

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,1 +1,13 @@
-// Your code goes here
+let a = +prompt('enter first side length');
+let b = +prompt('enter second side length');
+let c = +prompt('enter third side length');
+
+if (a === b && b === c) {
+    confirm('Equivalent triangle');
+} else if (a === b || b === c || c === a) {
+    confirm('Isosceles triangle');
+} else if (a < b + c && b < a + c && c < a + b) {
+    confirm('Normal triangle');
+} else {
+    confirm("Triangle doesn't exist")
+}
