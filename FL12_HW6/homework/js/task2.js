a9ca1d2be7a98b744557a62430d6e116e4d9f6c5
VasylKravchenko <vasylkravchenko@gmail.com>
2019-12-20 18:07:55 +0200

    hw6 task2 validation, eslint rename

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,8 +1,14 @@
-let a = +prompt('enter first side length');
-let b = +prompt('enter second side length');
-let c = +prompt('enter third side length');
+let a = prompt('enter first side length');
+let b = prompt('enter second side length');
+let c = prompt('enter third side length');
 
-if (a === b && b === c) {
+if (a !== Number || a !== '' || a <= 0) {
+    confirm('input values should be ONLY numbers');
+} else if (b !== Number || b !== '' || b <= 0) {
+    confirm('input values should be ONLY numbers');
+} else if (c !== Number || c !== '' || c <= 0) {
+    confirm('input values should be ONLY numbers');
+} else if (a === b && b === c) {
     confirm('Equivalent triangle');
 } else if (a === b || b === c || c === a) {
     confirm('Isosceles triangle');
