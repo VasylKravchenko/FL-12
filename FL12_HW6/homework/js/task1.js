3b09adec283978b61c854b35bd8f62d341a826ec
VasylKravchenko <vasylkravchenko@gmail.com>
2019-12-21 14:15:28 +0200

    Homework 6 Into to JS

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,1 +1,20 @@
-// Your code goes here
+let a = +prompt('enter first value');
+let b = +prompt('enter second value');
+let c = +prompt('enter third value');
+
+let two = 2;
+let four = 4;
+let discr = b * b - four * a * c;
+let sqrDiscr = Math.sqrt(discr);
+
+if (a === 0 && b === 0 && c === 0) {
+    console.log('invalid input data');
+} else if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    console.log('invalid input data');
+} else if (discr < 0) {
+    console.log('no solution');
+} else if (discr === 0) {
+    console.log(`x = ${(-b - sqrDiscr) / (two * a)}`);
+} else if (discr > 0) {
+    console.log(`x1 = ${(-b + sqrDiscr) / (two * a)} and x2 = ${(-b - sqrDiscr) / (two * a)}`)
+}
