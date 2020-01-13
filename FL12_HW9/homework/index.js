8ccc466dbd3df5bb772183fa9eb78723787a2fa9
VasylKravchenko <vasylkravchenko@gmail.com>
2020-01-14 01:49:30 +0200

    task1 convert

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -1,1 +1,16 @@
-// Your code goes here
+// task 1
+function convert() {
+    let valArr = [];
+    for (let i = 0; i < arguments.length; i++) {
+
+        if (typeof arguments[i] === 'number') {
+            arguments[i] = String(arguments[i]);
+        } else if (typeof arguments[i] === 'string') {
+            arguments[i] = Number(arguments[i]);
+        }
+        valArr.push(arguments[i]);
+    }
+    return valArr;
+}
+
+convert('1', 2, 3, '4');
